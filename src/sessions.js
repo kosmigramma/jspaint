39248be96c857076385e250921e6b9ad79013464
Haukur Rosinkranz <hauxir@gmail.com>
2021-01-07 17:49:42 +0100

    set production url

--- src/sessions.js
+++ src/sessions.js
@@ -191,7 +191,7 @@
 	class MultiUserSession {
 		constructor(session_id) {
 			this.id = session_id;
-	                this.socket = io('http://localhost:5000', {transports: ['websocket']});
+	                this.socket = io('https://backend.paint.kosmi.io', {transports: ['websocket']});
                         this.socket.emit("join", session_id);
 			update_title();
 			file_name = `[${this.id}]`;
